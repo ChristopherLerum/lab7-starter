de3be4358b860b2a5a82a04442adaa980ad78150
ChristopherLerum <146862377+ChristopherLerum@users.noreply.github.com>
2024-05-21 23:38:40 -0700

    Explore

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -58,9 +58,9 @@ function saveRecipesToStorage(recipes) {
 	// B1. TODO - Complete the functionality as described in this function
 	//            header. It is possible in only a single line, but should
 	//            be no more than a few lines.
-	let myRecipes = JSON.parse(localStorage.getItem('recipes'));
-	myRecipes[myRecipes.length] = recipes;
-	localStorage.setItem('recipes', JSON.stringify(myRecipes));
+	// let myRecipes = JSON.parse(localStorage.getItem('recipes'));
+	// myRecipes[myRecipes.length] = recipes;
+	localStorage.setItem('recipes', JSON.stringify(recipes));
 }
 
 /**
@@ -69,26 +69,53 @@ function saveRecipesToStorage(recipes) {
  */
 function initFormHandler() {
 	// B2. TODO - Get a reference to the <form> element
-	let myMain = document.querySelector('form');
+	let myForm = document.querySelector('form');
 	// B3. TODO - Add an event listener for the 'submit' event, which fires when the
 	//            submit button is clicked
-	document.querySelector('button').addEventListener('click', (event) => {
+	document.querySelector('button[type=submit]').addEventListener('click', () => {
+		// Steps B4-B9 will occur inside the event listener from step B3
+		// B4. TODO - Create a new FormData object from the <form> element reference above
+		let myData = new FormData(myForm, document.querySelector('button[type=submit]'));
 		
+		// B5. TODO - Create an empty object (we'll refer to this object as recipeObject to
+		//            make this easier to read), and then extract the keys and corresponding
+		//            values from the FormData object and insert them into recipeObject
+		let recipeObject = {};
+		for (const [key, value] of myData.entries()) {
+			recipeObject[key] = value;
+			// console.log(`${key}: ${value}`);
+		}
+
+		// B6. TODO - Create a new <recipe-card> element
+		var myNewRecipe = document.createElement('recipe-card');
+
+		// B7. TODO - Add the recipeObject data to <recipe-card> using element.data
+		myNewRecipe.data = recipeObject;
+
+		// B8. TODO - Append this new <recipe-card> to <main>
+		document.querySelector('main').append(myNewRecipe);
+
+		// B9. TODO - Get the recipes array from localStorage, add this new recipe to it, and
+		//            then save the recipes array back to localStorage
+		let myRecipes = getRecipesFromStorage();
+		myRecipes[myRecipes.length] = recipeObject;
+		saveRecipesToStorage(myRecipes);
+
 	});
-	// Steps B4-B9 will occur inside the event listener from step B3
-	// B4. TODO - Create a new FormData object from the <form> element reference above
-	// B5. TODO - Create an empty object (we'll refer to this object as recipeObject to
-	//            make this easier to read), and then extract the keys and corresponding
-	//            values from the FormData object and insert them into recipeObject
-	// B6. TODO - Create a new <recipe-card> element
-	// B7. TODO - Add the recipeObject data to <recipe-card> using element.data
-	// B8. TODO - Append this new <recipe-card> to <main>
-	// B9. TODO - Get the recipes array from localStorage, add this new recipe to it, and
-	//            then save the recipes array back to localStorage
+
 	// B10. TODO - Get a reference to the "Clear Local Storage" button
+	let myClear = document.querySelector('button[class=danger]');
+
 	// B11. TODO - Add a click event listener to clear local storage button
-	// Steps B12 & B13 will occur inside the event listener from step B11
-	// B12. TODO - Clear the local storage
-	
-	// B13. TODO - Delete the contents of <main>
+	myClear.addEventListener('click', () => {
+		// Steps B12 & B13 will occur inside the event listener from step B11
+		// B12. TODO - Clear the local storage
+		localStorage.clear();
+		
+		// B13. TODO - Delete the contents of <main>
+		let myMain = document.querySelector('main');
+		while (myMain.firstChild){
+			myMain.removeChild(myMain.firstChild);
+		}
+	});
 }
